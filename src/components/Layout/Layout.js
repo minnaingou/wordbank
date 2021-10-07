f5f047ddeac01c5d4776ed1c2189aed64e1fc562
Min Naing Oo <minnaingoo@yomabank.com>
2021-10-07 13:41:45 +0630

    Outsourced drawer items to Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Container from "@mui/material/Container";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import InfoIcon from "@mui/icons-material/Info";
 
 import BottomNavigator from "../UI/ButtomNavigator/BottomNavigator";
 import BottomDrawer from "../UI/BottomDrawer/BottomDrawer";
@@ -7,6 +9,25 @@ import TopBar from "../UI/TopBar/TopBar";
 
 const Layout = (props) => {
   const [showDrawer, setShowDrawer] = useState(false);
+
+  const drawerItems = [
+    {
+      label: "Statistics",
+      icon: <BarChartIcon />,
+      clicked: () => {
+        setShowDrawer(false);
+        console.log("stats clicked");
+      },
+    },
+    {
+      label: "About",
+      icon: <InfoIcon />,
+      clicked: () => {
+        setShowDrawer(false);
+        console.log("about clicked");
+      },
+    },
+  ];
 
   const onToggleHandler = () => {
     setShowDrawer(!showDrawer);
@@ -17,7 +38,7 @@ const Layout = (props) => {
       <TopBar />
       <main>{props.children}</main>
       <BottomNavigator clickedMore={onToggleHandler} />
-      <BottomDrawer show={showDrawer} />
+      <BottomDrawer show={showDrawer} items={drawerItems} />
     </Container>
   );
 };
--- src/components/UI/BottomDrawer/BottomDrawer.js
+++ src/components/UI/BottomDrawer/BottomDrawer.js
@@ -5,23 +5,15 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 
 const BottomDrawer = (props) => {
-
   const list = () => (
-    <Box
-      sx={{ width: "auto", paddingBottom: 8 }}
-      role="presentation"
-    >
+    <Box sx={{ width: "auto", paddingBottom: 8 }} role="presentation">
       <List>
-        {["Statistics", "About"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
+        {props.items.map((item, index) => (
+          <ListItem key={index} button onClick={item.clicked}>
+            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemText primary={item.label} />
           </ListItem>
         ))}
       </List>
@@ -30,7 +22,7 @@ const BottomDrawer = (props) => {
 
   return (
     <div>
-      <Drawer anchor="bottom" open={props.show} onClose={props.clicked}>
+      <Drawer anchor="bottom" open={props.show}>
         {list()}
       </Drawer>
     </div>
